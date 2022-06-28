2d803a44b4d74422078cf51d87d39a8ff96bc8a3
syedmfuad <syed.m.fuad@gmail.com>
2022-06-28 14:09:05 -0500

    Added codes for >2 submarkets

--- atlanta_submarket_4.R
+++ atlanta_submarket_4.R
@@ -1,29 +1,9 @@
-#https://rural-urban.eu/sites/default/files/05_Spatial%20Autocorrelation%20and%20the%20Spatial%20Durbin%20Model_Eilers.pdf
-#https://d-nb.info/1188635956/34
-#http://rstudio-pubs-static.s3.amazonaws.com/5027_52298866e7924b18b54e5c9a0a21b450.html
-#https://stats.stackexchange.com/questions/149415/how-do-i-interpret-lagsarlm-output-from-rs-spdep
-#https://rpubs.com/corey_sparks/109650
-#https://rpubs.com/corey_sparks/108130
-#https://www.stata.com/training/webinar_series/spatial-autoregressive-models/spatial/resource/spillover.html
-
-library(tidyverse)
-library(spdep)
-library(spatialreg)
-library(rgdal)
-library(rgeos)
-library(dplyr)
-library(sp)
-library(rgeos)
-library(geosphere)
-library(rstatix)
-
-#example with ethiopia
-
 rm(list=ls())
 
 set.seed(12345)
 data <- read.csv("atlanta single family.csv")
 data$sqft <- as.numeric(data$sqft)
+data <- subset(data, sqft>0)
 
 data$lon <- -1*data$lon
 data$plon <- 0 #84.3563
@@ -72,35 +52,28 @@ X <- cbind(1, I(log(data$calcacres)), I(log((data$ENGMeanScaleScore15)/100)), I(
 Z <- cbind(1,I(data$ENGMeanScaleScore15)/100, I(data$calcacres), I(data$median_income)/10000, I(data$age), I(data$pct_white), I(data$pct_black),
            I(data$pct_collegeDegree), I(data$median_house_value)/100000, I(data$HHsize))
 
-
 ols_agg <- lm(Y~X-1);
 summary(ols_agg)
 
-k = ncol(X)
-n=nrow(X)
-beta = matrix(NA,nrow=n,ncol=k)
-sigma = c(1,rep(NA,n))
-psi = rep(NA,n)
-
-set.seed(12345)
-
 #starting values for the hedonic estimates/betas for each type i.e. for mixing algorithm
 
-beta_start <- matrix(ols_agg$coef,(2*ncol(X)),1);  
+types <- 4;
+
+beta_start <- matrix(ols_agg$coef,(types*ncol(X)),1);  
 
 #starting values for the gamma estimates for the demographic variables
 
 gamma_start <- matrix(0.01,(1*ncol(Z)),1);
 
 #starting values for sigma
-sigma_start <- matrix(sqrt(mean(ols_agg$residuals^2)),2,1)     
+sigma_start <- matrix(sqrt(mean(ols_agg$residuals^2)),types,1)     
 
 #collecting initializing values
 
 val_start <- c(beta_start,gamma_start,sigma_start);
 
 vals <- val_start;
-types <- 2;
+types <- 4;
 
 #convergence criteria comparing new and old estimates:
 
@@ -128,7 +101,6 @@ par <- val_start
 
 FnOne <- function(par,x,y) 
 {
-  #par[1] <- ifelse(par[1]>0, par[1], -1*par[1])
   dnorm(y, mean=x%*%par[-1], sd = par[1], log=FALSE)   
 }
 
@@ -143,7 +115,6 @@ FnTwo <- function(par,d,x,y)
   {	
     pdy[,i] <- FnOne(c(s[i],b[((i-1)*niv+1):(i*niv)]),X,Y)        
   }
-  #pdy[pdy < 0.0005] <- 0.05
   sum(d*log(pdy))
 }
 
@@ -160,7 +131,9 @@ FnFour <- function(par,d,z,y)
 {
   L <- matrix(0,n,j) 
   L[,1] <- 1
-  for (m in 1:(j-1))   
+  L[,3] <- 1
+  L[,4] <- 1
+  for (m in 1:(j-3))   
   { 
     L[,(m+1)] <- FnThree(par[((m-1)*gvs+1):(m*gvs)],z)     
   }
@@ -173,8 +146,10 @@ FnFour <- function(par,d,z,y)
 FMM <- function(par,X,Z,y) 
 {
   b <- par[1:(j*niv)]; 
-  g <- par[(j*niv+1):((j*(niv+gvs)-gvs))]; 
-  s <- par[-(1:(j*(niv+gvs)-gvs))];
+  #g <- par[(j*niv+1):((j*(niv+gvs)-gvs))]; 
+  g <- par[(j*niv+1):(length(par)-types)];
+  #s <- par[-(1:(j*(niv+gvs)-gvs))];
+  s <- par[-(1:(length(par)-types))];
   L <- matrix(0,n,j); 
   f <- L; 
   d <- L;
@@ -194,9 +169,11 @@ FMM <- function(par,X,Z,y)
     { 
       f[,i] <- FnOne(c(s[i],b[,i]),X,Y)		
     }
-    for (i in 1:(j-1)) 
+    for (i in 1:(j-3)) 
     { 
       L[,1] <- 0
+      L[,i+1] <- 0
+      L[,i+2] <- 0
       L[,(i+1)] <- Z%*%g[((i-1)*gvs+1):(i*gvs)] 
     }
     
@@ -258,28 +235,37 @@ d <- mix$i_type
 
 b <- mix$vals_fin[1:(j*niv)]; 
 
-g <- mix$vals_fin[(j*niv+1):((j*(niv+gvs)-gvs))]; 
+#g <- mix$vals_fin[(j*niv+1):((j*(niv+gvs)-gvs))];
+g <- mix$vals_fin[(j*niv+1):(length(par)-types)];
 
-s <- mix$vals_fin[-(1:(j*(niv+gvs)-gvs))];
+#s <- mix$vals_fin[-(1:(j*(niv+gvs)-gvs))];
+s <- mix$vals_fin[-(1:(length(par)-types))];
 
 b <- matrix(b,niv,j);
 
 b1 <- matrix(b,(niv*j),1); 
-
 par3 <- c(b1,s);
+
 
 ########## EXTRA ##########
 
 datanew <- cbind(newdata, d)
-datanew$prob <- ifelse(datanew$`1` > datanew$`2`, 1, 2)
+datanew$prob <- colnames(datanew[, c(42:45)])[apply(datanew[, c(42:45)],1,which.max)]
+datanew$prob <- as.numeric(datanew$prob)
 
 data1 <- subset(datanew, prob==1)
 data2 <- subset(datanew, prob==2)
+data3 <- subset(datanew, prob==3)
+data4 <- subset(datanew, prob==4)
 
-data1 %>% get_summary_stats(ENGMeanScaleScore15, calcacres, median_income, age, pct_white, pct_black, 
-                            pct_collegeDegree, median_house_value, HHsize, type = "mean_sd") -> sumdata1
-data2 %>% get_summary_stats(ENGMeanScaleScore15, calcacres, median_income, age, pct_white, pct_black, 
-                            pct_collegeDegree, median_house_value, HHsize, type = "mean_sd") -> sumdata2
+data1 %>% get_summary_stats(salesprice, ENGMeanScaleScore15, calcacres, median_income, age, pct_white, pct_black, 
+                            pct_collegeDegree, median_house_value, HHsize, sqft, type = "mean_sd") -> sumdata1
+data2 %>% get_summary_stats(salesprice, ENGMeanScaleScore15, calcacres, median_income, age, pct_white, pct_black, 
+                            pct_collegeDegree, median_house_value, HHsize, sqft, type = "mean_sd") -> sumdata2
+data3 %>% get_summary_stats(salesprice, ENGMeanScaleScore15, calcacres, median_income, age, pct_white, pct_black, 
+                            pct_collegeDegree, median_house_value, HHsize, sqft, type = "mean_sd") -> sumdata3
+data4 %>% get_summary_stats(salesprice, ENGMeanScaleScore15, calcacres, median_income, age, pct_white, pct_black, 
+                            pct_collegeDegree, median_house_value, HHsize, sqft, type = "mean_sd") -> sumdata4
 
 LL <- FnTwo(par3,d=d,x=X,y=Y) + FnFour(g,d=d,z=Z,y=Y);
 AIC <- -2*LL+2*niv
@@ -294,35 +280,23 @@ X <- cbind(1, I(log(data$calcacres)), I(log((data$ENGMeanScaleScore15)/100)), I(
            I(data$age*data$age)/1000, I(data$totbath), I(data$stories), I(data$centheat), I(data$fourthquart), I(data$pct_renter_occupied),
            I(data$sqft)/1000, I(data$lon-data$plon), I(data$lat-data$plat), I((data$lon-data$plon)^2)/1000, I((data$lat-data$plat)^2)/1000)
 
-pred <- (X%*%b[,1]*Ds[,1] + X%*%b[,2]*Ds[,2])*100000
+pred <- (X%*%b[,1]*Ds[,1] + X%*%b[,2]*Ds[,2] + X%*%b[,3]*Ds[,3] + X%*%b[,4]*Ds[,4])*100000
 
 resid <- Y-pred
 rmse2 <- sqrt(mean((resid)^2))
 
 
 
+
+
 #standard errors
 
-#this is original
-
-par3 <- c(-2.109626e+05,  4.460316e-01,  1.270029e+01,  5.553325e-01,  3.686364e-03,  2.205276e-02,  8.017481e-01,  1.067674e-01, -4.144621e-03,  1.743981e-01,
-          -4.281453e-03,  6.328598e-01, -4.762233e+03,  6.200320e+02, -2.830400e+04, -9.039645e+03, -2.109611e+05,  2.116792e+00,  1.354729e+01,  9.884247e-01,
-          -2.360048e-02,  3.985402e-01,  1.470050e+00,  5.869073e-01, -1.061358e-01, -7.041625e-02, -5.112193e-03,  9.428571e-01, -4.762243e+03,  6.199476e+02,
-          -2.830388e+04, -9.040922e+03,  1.176798e+00,  3.537552e+00)
-
-#this is for bayesian (this works)
-
-par3 <- c(-2.109596e+05,  4.686432e-01,  1.175717e+01,  5.572309e-01,  1.033144e-03,  4.809619e-05,  7.988188e-01,  1.340362e-02, -4.321235e-04,  1.722443e-01,
-          -4.724813e-03,  7.169791e-01, -4.762159e+03,  6.200727e+02, -2.830370e+01, -9.038494e+00, -2.109625e+05,  2.078251e+00,  1.285127e+01,  1.076832e+00,
-          -2.017951e-02,  3.609336e-04,  1.472500e+00,  6.867002e-01, -8.974575e-02, -5.156546e-02, -3.996879e-03,  9.197446e-01, -4.762208e+03,  6.201338e+02,
-          -2.830390e+01, -9.041939e+00,  1.158014e+00,  3.573728e+00)
-
-
-beta_opt <- optim(par3,FnTwo,d=d,x=X,y=Y,control=list(fnscale=-1,maxit=100000),hessian=TRUE)
-
+beta_opt <- optim(par3,FnTwo,d=d,x=X,y=Y,control=list(fnscale=-1,maxit=10000, ndeps=rep(0.00005, 28)),
+                  hessian=TRUE, method = c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN", "Brent"))
 b <- matrix(beta_opt$par[1:(j*niv)],niv,j); 
 bse1 <- sqrt(-diag(solve(beta_opt$hessian[1:niv,1:niv])))
 bse2 <- sqrt(-diag(solve(beta_opt$hessian[(niv+1):(2*niv),(niv+1):(2*niv)])))
+bse3 <- sqrt(-diag(solve(beta_opt$hessian[(niv*2+1):(3*niv),(niv*2+1):(3*niv)])))
 
 s <- beta_opt$par[(j*niv+1):(j*(niv+1))]
 
@@ -336,23 +310,18 @@ par2 <- c(par2,s)
 LL <- FnTwo(par2,d=d,x=X,y=Y) + FnFour(g,d=d,z=Z,y=Y);
 AIC <- -2*LL+2*niv
 
-Ds=d;
-beta=b;
-bse=cbind(bse1,bse2);
+Ds=d; #probabilities of belonging in submarkets
+beta=b; #beta estimates
+bse=cbind(bse1,bse2,bse3); #standard errors
 gamma=cbind(g[1:gvs],g[(gvs+1):(2*gvs)]);
 gse=cbind(gse1);
 
-data$yhat <- ((1*beta[1,1]+data$calcacres*beta[2,1]+data$stories*beta[3,1]+
-                 data$age*beta[4,1]+data$centheat*beta[5,1]+data$totbath*beta[6,1]+data$sqft*beta[7,1])*Ds[,1] +
-                (1*beta[1,2]+data$calcacres*beta[2,2]+data$stories*beta[3,2]+
-                   data$age*beta[4,2]+data$centheat*beta[5,2]+data$totbath*beta[6,2]+data$sqft*beta[7,2])*Ds[,2])
-data$resid <- data$salesprice-data$yhat
-rmse2 <- sqrt(mean((data$resid)^2))
+row_nombre <- c("Intercept","Lot Size","Stories", "House Age", "Central Heat", "Total Bath")
 
-
-
-
-
-
-
-
+write.table(b, file= paste0(path,"Beta.csv"),quote = FALSE, row.names= row_nombre, col.names=col_nombre, sep=",")
+write.table(bse, file= paste0(path,"Bse.csv"),quote = FALSE, row.names= row_nombre, col.names=col_nombre, sep=",")
+write.table(LL, file= paste0(path,"LL.csv"),quote = FALSE, row.names= TRUE, col.names=TRUE, sep=",")
+write.table(s, file= paste0(path,"S.csv"),quote = FALSE, row.names= TRUE, col.names=TRUE, sep=",")
+write.table(gse1, file= paste0(path,"Gse.csv"),quote = FALSE, row.names= TRUE, col.names=TRUE, sep=",")
+write.table(gamma, file= paste0(path,"Gamma.csv"),quote = FALSE, row.names= TRUE, col.names=TRUE, sep=",")
+write.table(d, file= paste0(path,"Dhats.csv"),quote = FALSE, row.names= TRUE, col.names=col_nombre, sep=",")
